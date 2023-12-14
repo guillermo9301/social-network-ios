4d44802d2608b6408c653111771577acd2ebde19
Guillermo Anticona <guille.anticonagmail.com>
2023-12-14 09:24:02 -0800

    Initial Commit

---
+++ examen-final/examen_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  examen_finalApp.swift
+//  examen-final
+//
+//  Created by Guillermo on 14/12/23.
+//
+
+import SwiftUI
+
+@main
+struct examen_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
